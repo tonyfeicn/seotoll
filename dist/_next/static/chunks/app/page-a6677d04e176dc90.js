7480448ceb93e56ec99e77dd8f2a4ad2ccc0940d
Tonyfeicn <tony@hotel.ai>
2026-06-12 14:56:07 +0800

    fresh deploy with correct paths

---
+++ dist/_next/static/chunks/app/page-a6677d04e176dc90.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{1733:function(n,u,e){Promise.resolve().then(e.bind(e,8212))},8212:function(n,u,e){"use strict";e.r(u),e.d(u,{default:function(){return f}});var t=e(7437),r=e(2790);function f(){return(0,t.jsx)(r.default,{})}}},function(n){n.O(0,[790,971,23,744],function(){return n(n.s=1733)}),_N_E=n.O()}]);
